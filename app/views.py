8d79146963c213413583790a7acef7362b8597af
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-02-13 23:06:11 -0500

    page-addition/profile

--- app/views.py
+++ app/views.py
@@ -1,31 +1,67 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
 # Routing for your application.
 ###
 
-@app.route('/')
+
+@app.route("/")
 def home():
     """Render website's home page."""
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/about/')
+@app.route("/about/")
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    user = {
+        "name": "Jamari McFarlane",
+        "at": "jtg",
+        "image_src": url_for("static", filename="images/jtg.jpg"),
+        "location": "Kingston, Jamaica",
+        "joined": datetime.date(2024, 1, 31),
+        "biography": "I am a software developer who primarily codes with the MERN Stack + GraphQL and is currently learning flask and SQL for my latest job (COINCIDENCE??). Gonna use this for my personal portfolio btw.",
+        "followers": 999,
+        "following": 0,
+        "posts": 56,
+    }
+    return render_template("about.html", name=user["name"])
 
 
-###
-# The functions below should be applicable to all Flask apps.
-###
+@app.route("/profile/")
+def profile():
+    user = {
+        "name": "Jamari McFarlane",
+        "at": "jtg",
+        "image_src": url_for("static", filename="images/jtg.jpg"),
+        "location": "Kingston, Jamaica",
+        "joined": datetime.date(2024, 1, 31),
+        "biography": "I am a software developer who primarily codes with the MERN Stack + GraphQL and is currently learning flask and SQL for my latest job (COINCIDENCE??). Gonna use this for my personal portfolio btw.",
+        "followers": 999,
+        "following": 0,
+        "posts": 56,
+    }
+    return render_template(
+        "profile.html",
+        name=user["name"],
+        image_src=user["image_src"],
+        at=user["at"],
+        location=user["location"],
+        joined=format_date_joined(user["joined"]),
+        biography=user["biography"],
+        followers=user["followers"],
+        following=user["following"],
+        posts=user["posts"],
+    )
 
-@app.route('/<file_name>.txt')
+
+@app.route("/<file_name>.txt")
 def send_text_file(file_name):
     """Send your static text file."""
-    file_dot_text = file_name + '.txt'
+    file_dot_text = file_name + ".txt"
     return app.send_static_file(file_dot_text)
 
 
@@ -36,12 +72,16 @@ def add_header(response):
     and also tell the browser not to cache the rendered page. If we wanted
     to we could change max-age to 600 seconds which would be 10 minutes.
     """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
+    response.headers["X-UA-Compatible"] = "IE=Edge,chrome=1"
+    response.headers["Cache-Control"] = "public, max-age=0"
     return response
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
+
+
+def format_date_joined(date):
+    return date.strftime("%b, %Y")
